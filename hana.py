a7c3bfbff4e3074cfdb634610a6cd403c86b088c
Andy Wang <cbeuw.andy@gmail.com>
2020-02-23 13:53:24 +0000

    SSASHJHHJSKHKSHMSJJSSASHKSJDSJHSSDSHJSSASJDSHMSKSJHHKSHJ

--- hana.py
+++ hana.py
@@ -1,10 +1,21 @@
 import sys
+from abc import abstractmethod
 from typing import *
 
 
 class EncodingTree:
+    pass
+
+
+class Leaf(EncodingTree):
     leaf: chr
-    children: Dict[chr, 'EncodingTree']
+
+    def __init__(self, lf):
+        self.leaf = lf
+
+
+class Node(EncodingTree):
+    children: Dict[chr, EncodingTree]
     encoding_table: Dict[chr, chr]
 
     def __enter__(self):
@@ -13,33 +24,34 @@ class EncodingTree:
     def __exit__(self, exc_type, exc_val, exc_tb):
         return
 
-    def __init__(self, lf='', children=None):
-        self.leaf = lf
+    def __init__(self, children=None):
         if children is None:
             self.children = {}
         else:
             self.children = children
         self.encoding_table = {}
 
-    def __setitem__(self, key: str, value: 'EncodingTree'):
+    def __setitem__(self, key: str, value: EncodingTree):
         if len(key) == 1:
             self.children[key] = value
         else:
-            if self.leaf is not '':
-                raise Exception('node cannot have leaf value')
-            self.children[key[0]][key[1:]] = value
+            child = self.children[key[0]]
+            if isinstance(child, Node):
+                child[key[1:]] = value
+            else:
+                raise Exception("cannot add a child to a leaf")
 
-    def __getitem__(self, item: str) -> chr:
-        if item == "":
-            return self.leaf
+    def __getitem__(self, item: str) -> EncodingTree:
+        if item == '':
+            return self
         else:
             return self[item[1:]]
 
     def build_encoding_table(self) -> Dict:
         for key, child in self.children.items():
-            if child.leaf != '':
+            if isinstance(child, Leaf):
                 self.encoding_table[child.leaf] = key
-            else:
+            elif isinstance(child, Node):
                 sub_table = child.build_encoding_table()
                 for c in sub_table.keys():
                     self.encoding_table[c] = key
@@ -49,99 +61,99 @@ class EncodingTree:
         return ''.join(self.hanafy_chr(c) for c in text)
 
     def hanafy_chr(self, c: chr) -> str:
-        if self.leaf == c:
-            return ""
-        else:
-            key = self.encoding_table[c]
-            return key + self.children[key].hanafy_chr(c)
+        key = self.encoding_table[c]
+        child = self.children[key]
+        if isinstance(child, Leaf):
+            return key
+        elif isinstance(child, Node):
+            return key + child.hanafy_chr(c)
 
     def dehana(self, hanaspeak: str) -> str:
         return self.__dehana(hanaspeak, self)
 
-    def __dehana(self, hanaspeak: str, root: 'EncodingTree') -> str:
+    def __dehana(self, hanaspeak: str, root: 'Node') -> str:
         if len(hanaspeak) == 0:
             return ""
-        cur = hanaspeak[0]
-        child = self.children[cur]
-        if child.leaf != '':
+        child = self.children[hanaspeak[0]]
+        if isinstance(child, Leaf):
             return child.leaf + root.dehana(hanaspeak[1:])
-        else:
+        elif isinstance(child, Node):
             return child.__dehana(hanaspeak[1:], root)
 
 
-def build_tree() -> EncodingTree:
-    root = EncodingTree()
+def build_tree() -> Node:
+    root = Node()
 
-    with EncodingTree() as S:
+    with Node() as S:
         root['S'] = S
 
-        S['K'] = EncodingTree('a')
+        S['K'] = Leaf('a')
 
-        with EncodingTree() as H:
+        with Node() as H:
             S['H'] = H
 
-            H['J'] = EncodingTree('e')
-            H['M'] = EncodingTree('t')
-            H['K'] = EncodingTree(' ')
+            H['J'] = Leaf('e')
+            H['M'] = Leaf('t')
+            H['K'] = Leaf(' ')
 
-        with EncodingTree() as J:
+        with Node() as J:
             S['J'] = J
 
-            J['J'] = EncodingTree('o')
-            J['D'] = EncodingTree('i')
-            J['H'] = EncodingTree('n')
+            J['J'] = Leaf('o')
+            J['D'] = Leaf('i')
+            J['H'] = Leaf('n')
 
-        with EncodingTree() as S2:
+        with Node() as S2:
             S['S'] = S2
 
-            S2['H'] = EncodingTree('s')
-            S2['A'] = EncodingTree('r')
-            S2['D'] = EncodingTree('h')
+            S2['H'] = Leaf('s')
+            S2['A'] = Leaf('r')
+            S2['D'] = Leaf('h')
 
-    with EncodingTree() as J:
+    with Node() as J:
         root['J'] = J
 
-        J['S'] = EncodingTree('d')
+        J['S'] = Leaf('d')
 
-        with EncodingTree() as D:
+        with Node() as D:
             J['D'] = D
 
-            D['A'] = EncodingTree('l')
-            D['N'] = EncodingTree('u')
-            D['J'] = EncodingTree('\'')
+            D['A'] = Leaf('l')
+            D['N'] = Leaf('u')
+            D['J'] = Leaf('\'')
 
-    with EncodingTree() as H:
+    with Node() as H:
         root['H'] = H
 
-        H['K'] = EncodingTree('c')
+        H['K'] = Leaf('c')
 
-        with EncodingTree() as H2:
+        with Node() as H2:
             H['H'] = H2
 
-            H2['S'] = EncodingTree('m')
-            H2['J'] = EncodingTree('f')
-            H2['K'] = EncodingTree('y')
+            H2['S'] = Leaf('m')
+            H2['J'] = Leaf('f')
+            H2['K'] = Leaf('y')
 
-    with EncodingTree() as K:
+    with Node() as K:
         root['K'] = K
 
-        K['S'] = EncodingTree('w')
+        K['S'] = Leaf('w')
 
-        with EncodingTree() as J:
+        with Node() as J:
             K['J'] = J
 
-            with EncodingTree() as J2:
+            with Node() as J2:
                 J['J'] = J2
 
-                J2['K'] = EncodingTree('g')
-                J2['M'] = EncodingTree('p')
-                J2['S'] = EncodingTree('b')
-    root['D'] = EncodingTree('v')
-    root['A'] = EncodingTree('k')
-    root['M'] = EncodingTree('x')
-    root['G'] = EncodingTree('q')
-    root['L'] = EncodingTree('j')
-    root['E'] = EncodingTree('z')
+                J2['K'] = Leaf('g')
+                J2['M'] = Leaf('p')
+                J2['S'] = Leaf('b')
+    root['D'] = Leaf('v')
+    root['A'] = Leaf('k')
+    root['M'] = Leaf('x')
+    root['G'] = Leaf('q')
+    root['L'] = Leaf('j')
+    root['E'] = Leaf('z')
 
     root.build_encoding_table()
     return root
